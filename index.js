a2457754917170cf2a67b34b6f6993c7133627cf
CourtneyFradreck <courtaga@gmail.com>
2024-02-13 22:55:09 +0200

    added some variables

---
+++ index.js
@@ -0,0 +1,4 @@
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const dob = document.getElementById("dob");
+const mobileNumber = document.getElementById("mobileNumber");
