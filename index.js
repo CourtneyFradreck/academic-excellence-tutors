7e27516520549331b9dd8f643a439f9e91b8a215
CourtneyFradreck <courtaga@gmail.com>
2024-02-28 22:03:20 +0200

    deleted index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const dob = document.getElementById("dob");
-const mobileNumber = document.getElementById("mobileNumber");
