12d63832f9a8442fa192aa8289ffd5cdc1af8c0e
CourtneyFradreck <courtaga@gmail.com>
2024-03-07 21:36:25 +0200

    changed var to let

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mobileNumber =document.getElementById("mobileNumber");
+let mobileNumber =document.getElementById("mobileNumber");
 function check(mobileNumber) {
     if (mobileNumber.value.length < 10) {
         mobileNumber.setCustomValidity("Error: Mobile number should have 10 digits");
