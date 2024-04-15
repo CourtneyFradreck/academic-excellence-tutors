7defe1bf406b7fc174500e28999f1430df265287
icep0ps <tapsmuko@gmail.com>
2024-04-15 23:23:55 +0200

    fixed hamburger menu

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-let mobileNumber =document.getElementById("mobileNumber");
+let mobileNumber = document.getElementById('mobileNumber');
 function check(mobileNumber) {
-    if (mobileNumber.value.length < 10) {
-        mobileNumber.setCustomValidity("Error: Mobile number should have 10 digits");
-    } else {
-        mobileNumber.setCustomValidity("");
-    }
+  if (mobileNumber.value.length < 10) {
+    mobileNumber.setCustomValidity('Error: Mobile number should have 10 digits');
+  } else {
+    mobileNumber.setCustomValidity('');
+  }
 }
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navBar = document.getElementsByClassName("navbar")[0];
-toggleButton.addEventListener("click", () =>{
-    navBar.classList.toggle("active")
-})
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const closebtn = document.getElementById('close-btn');
+closebtn.addEventListener('click', () => {
+  navBar.classList.toggle('active');
+});
+const navBar = document.getElementsByClassName('navbar')[0];
+toggleButton.addEventListener('click', () => {
+  navBar.classList.toggle('active');
+});
