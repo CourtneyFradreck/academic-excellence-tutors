ca2f32c1420f1902468af7742943418424261718
Courtney Fradreck <courtaga@gmail.com>
2025-05-21 18:33:25 +0200

    remove database setup and form processing scripts

--- database/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `tutoring-school`;
-USE `tutoring-school`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` INT AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `message` VARCHAR(255) NOT NULL,
-);
